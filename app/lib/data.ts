01f9cba50ab0acbdfb5b841436c68aaa0bd68cf6
Mikey Curtis <mjcurtis@umail.ucsb.edu>
2024-04-06 13:37:55 -0700

    working on search

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -57,6 +57,7 @@ export async function fetchLatestInvoices() {
 
 export async function fetchCardData() {
   noStore();
+  await new Promise((resolve) => setTimeout(resolve, 3000));
   try {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
